9008ab1ac8e777aabcb908a356183ac07b959d01
shanawaz khan <shanawaz.khan@somaiya.edu>
2021-08-23 01:04:04 -0700

    header new added, PWA

---
+++ build/precache-manifest.0acd6e99fe9615657afbce13fca14e59.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d03f233d1e58a5a6f1dd38e80a27e51",
+    "url": "./index.html"
+  },
+  {
+    "revision": "922d71c5b0ccd9ca009b",
+    "url": "./static/css/2.63f31f8d.chunk.css"
+  },
+  {
+    "revision": "50137cba1fb2f9d5a572",
+    "url": "./static/css/main.3207a43c.chunk.css"
+  },
+  {
+    "revision": "922d71c5b0ccd9ca009b",
+    "url": "./static/js/2.cc361be6.chunk.js"
+  },
+  {
+    "revision": "50137cba1fb2f9d5a572",
+    "url": "./static/js/main.e73c0caf.chunk.js"
+  },
+  {
+    "revision": "dd87f1812c02eb6c1449",
+    "url": "./static/js/runtime-main.0a555640.js"
+  },
+  {
+    "revision": "1336f859da236dd0f5b5c4fbdca1f8d4",
+    "url": "./static/media/LoginPage.1336f859.png"
+  },
+  {
+    "revision": "ef632936eaa929e0c2efeca1ba36e464",
+    "url": "./static/media/Mobile-OTP.ef632936.svg"
+  },
+  {
+    "revision": "9ed353f739f15b6d6881d4efbaef190d",
+    "url": "./static/media/Referral Code grey.9ed353f7.svg"
+  },
+  {
+    "revision": "4809245658886b596ef0eda68d7f9487",
+    "url": "./static/media/ecosystem.48092456.png"
+  },
+  {
+    "revision": "39a7f830d00680b4a5cf8195307aa6af",
+    "url": "./static/media/landing.39a7f830.png"
+  },
+  {
+    "revision": "7465216e36c923457c0c8343da11b212",
+    "url": "./static/media/mobile.7465216e.svg"
+  }
+]);
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,7 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import Navbar from 'react-bootstrap/Navbar'
 import { useHistory } from "react-router";
+import {Nav,Container,NavDropdown} from "react-bootstrap"
 import "./Header.css";
 const Header = () => {
   const routerHistory = useHistory();
@@ -13,90 +14,31 @@ const Header = () => {
   };
   return (
     <div>
-      <nav className="navbar navbar-expand-lg color-gradiant navbar-light">
-        <br />
-        <br />
-        <Link to="/" className="navbar-brand ml-5 text-white" href="#">
-          Nuniyo
-        </Link>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav ml-auto mr-5">
-            <li className="nav-item active">
-              <Link
-                to="/VerifyContact"
-                className="nav-link text-white"
-                href="#"
-              >
-                Signup <span className="sr-only">(current)</span>
-              </Link>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link text-white" href="#">
-                About
-              </a>
-            </li>
-            {/* <li className="nav-item dropdown">
-              <a
-                className="nav-link text-white dropdown-toggle"
-                href="#"
-                id="navbarDropdown"
-                role="button"
-                data-toggle="dropdown"
-                aria-haspopup="true"
-                aria-expanded="false"
-              >
-                Products
-              </a>
-              <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-                <a className="dropdown-item" href="#">
-                  Action
-                </a>
-                <a className="dropdown-item" href="#">
-                  Another action
-                </a>
-                <div className="dropdown-divider"></div>
-                <a className="dropdown-item" href="#">
-                  Something else here
-                </a>
-              </div>
-            </li> */}
-            <li className="nav-item">
-              <a className="nav-link text-white " href="#">
-                Products
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link text-white " href="#">
-                Pricing
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link text-white " href="#">
-                Support
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link text-white " onClick={userEnd} href="#">
-                Logout
-              </a>
-            </li>
-          </ul>
-        </div>
-        <br />
-        <br />
-      </nav>
+<Navbar collapseOnSelect expand="lg" className="color-gradiant" variant="dark">
+  <Container>
+  <Navbar.Brand href="#home">Nuniyo</Navbar.Brand>
+  <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+  <Navbar.Collapse id="responsive-navbar-nav">
+    <Nav className="">
+      <Nav.Link href="/verifyContact">Signup</Nav.Link>
+      <Nav.Link href="#pricing">Pricing</Nav.Link>
+      {/* <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+        <NavDropdown.Divider />
+        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+      </NavDropdown> */}
+    </Nav>
+    <Nav>
+      <Nav.Link href="#deets">More deets</Nav.Link>
+      <Nav.Link eventKey={2} href="#memes">
+        Dank memes
+      </Nav.Link>
+    </Nav>
+  </Navbar.Collapse>
+  </Container>
+</Navbar>
     </div>
   );
 };
